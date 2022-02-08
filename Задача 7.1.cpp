daf9af76c885f188ae585281753a26c36ebf6c7e
BaldinAlexey <91282602+BaldinAlexey@users.noreply.github.com>
2022-02-08 18:49:24 +0500

    Add files via upload

---
+++ Задача 7.1.cpp
@@ -0,0 +1,21 @@
+﻿#include<iostream>
+#include<complex>
+using namespace std;
+
+complex< double > function(complex< double > a, complex< double > b)
+{
+	return a * b;
+}
+
+double function(double a, double b)
+{
+	return a * b;
+}
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	cout << "Результат вещественных чисел" << " " << function(5.456, 9.235) << endl;
+	cout << "Результат комплексных чисел" << " " << function(complex < double > (4, 9) , complex < double > (2, 8)) << endl;
+	return 0;
+}
