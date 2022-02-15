7afbea45997057f44975ef3e1952ff0ef05e4f4f
BaldinAlexey <91282602+BaldinAlexey@users.noreply.github.com>
2022-02-15 21:58:19 +0500

    Add files via upload

---
+++ Лаб.р. номер (7.2)/Задача 7.2.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include <stdarg.h>
+using namespace std;
+
+int Funcsum(int n, ...)
+{
+    int* parametr = &n;
+    parametr++;
+    int s = 0, a1 = 0, a2 = 0;
+    for (int i = 1; i < n; i++)
+    {   
+            a1 = (*parametr); //a1=parametr[1]
+            a2 = *(++parametr); //a2=parametr[2]
+            if (i % 2 == 0) //если чётное
+            {
+                s -= a1 * a2;
+            }
+            else
+            {
+                s += a1 * a2; //если нечётное
+            }
+    }
+    return s;
+}
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    cout <<"C количеством параметров 5  :"<< Funcsum(5, 1, 4, 9, 3, 6) << endl;
+    cout <<"C количеством параметров 10 :"<< Funcsum(10, 10, 11, 34, 4, 6, 2, 6, 7, 3, 15) << endl;
+    cout <<"C количеством параметров 12 :"<< Funcsum(12, 3, 56, 6, 2, 7, 1, 23, 21, 10, 15, 11, 46) << endl;
+    return 0;
+}
+
