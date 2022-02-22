386906797b5cf58625683315d1d1f98086f98d0d
BaldinAlexey <91282602+BaldinAlexey@users.noreply.github.com>
2022-02-22 21:07:14 +0500

    Add files via upload

---
+++ Ханойская Башня/Ханойская башня.cpp
@@ -0,0 +1,22 @@
+﻿#include <iostream>
+using namespace std;
+
+int Func(int n, int main, int next, int help)
+{
+	if (n > 0)
+	{
+		Func(n - 1, main, help, next);
+		cout << main << " Переходит в " << next << endl;
+		Func(n - 1, help, next, main);
+	}
+	return 0;
+}
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int n;
+	cout << "Введите количество надстроек башни: ";
+		cin >> n;
+	Func(n, 1, 3, 2);
+	return 0;
+}
