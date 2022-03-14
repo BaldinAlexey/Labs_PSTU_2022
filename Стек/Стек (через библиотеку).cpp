6001c029c5200d3df79cb42bf17619da9d76f2b5
BaldinAlexey <91282602+BaldinAlexey@users.noreply.github.com>
2022-03-15 00:53:57 +0500

    Add files via upload

---
+++ Стек/Стек (через библиотеку).cpp
@@ -0,0 +1,88 @@
+﻿#include <iostream>
+#include <stack>
+using namespace std;
+stack <char> s1;
+
+void make_point(int n)
+{
+    char x;
+    for (int i = n; i > 0; i--)
+    {
+        cout << "Элемент " << i << " ";
+        cin >> x;
+        cout << endl;
+        s1.push(char(x));
+    }
+}
+
+void print_point()
+{
+    stack <char> first;
+    first = s1;
+    while (!(s1.empty()))
+    {
+        cout << s1.top() << " ";
+
+        s1.pop();
+    }
+    cout << "\n";
+
+    s1 = first;
+}
+
+void add_point(char a, char x) {
+    stack <char> first;
+
+    while (!(s1.empty())) {
+
+        if (s1.top() == a) {
+            first.push(s1.top());
+            first.push(x);
+        }
+        else
+        {
+            first.push(s1.top());
+        }
+        s1.pop();
+    }
+
+    while (!(first.empty())) {
+        s1.push(first.top());
+        first.pop();
+    }
+}
+
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    char x, a;
+    cout << "Введите количество элементов стека: ";
+    int n;
+    cin >> n;
+    while (n <= 0)
+    {
+        cout << "Введите еще раз! ";
+        cin >> n;
+        cout << endl;
+    }
+    while (n >= 1)
+    {
+        make_point(n);
+        cout << "Созданный стек: ";
+        print_point();
+        cout << endl;
+        cout << "Выберете элемент, после которого будет стоять новый элемент: ";
+        cin >> a;
+        cout << endl;
+
+        cout << "Введите элемент, который будет стоять после указанного: ";
+        cin >> x;
+        add_point(a, x);
+        cout << endl;
+        cout << "Стек после преобразований: ";
+        print_point();
+        cout << endl << endl;
+        return 0;
+    }
+}
